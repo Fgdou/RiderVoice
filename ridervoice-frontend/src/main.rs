bf57d3ea6a178a988c0ac5e714f2b14162904f29
Fgdou <fabigoardou@gmail.com>
2025-08-26 19:59:20 +0100

    working map

--- ridervoice-frontend/src/main.rs
+++ ridervoice-frontend/src/main.rs
@@ -1,24 +1,65 @@
+use crate::components::{
+    control::{Cities, Control},
+    map_component::{City, MapComponent, Point},
+};
 use yew::prelude::*;
+mod components;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
+enum Msg {
+    SelectCity(City),
+}
+
+struct Model {
+    city: City,
+    cities: Cities,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        let aachen = City {
+            name: "Aachen".to_string(),
+            lat: Point(50.7597f64, 6.0967f64),
+        };
+        let stuttgart = City {
+            name: "Stuttgart".to_string(),
+            lat: Point(48.7784f64, 9.1742f64),
+        };
+        let cities: Cities = Cities {
+            list: vec![aachen, stuttgart],
+        };
+        let city = cities.list[0].clone();
+        Self { city, cities }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::SelectCity(city) => {
+                self.city = self
+                    .cities
+                    .list
+                    .iter()
+                    .find(|c| c.name == city.name)
+                    .unwrap()
+                    .clone();
+            }
         }
-    };
+        true
+    }
 
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let cb = ctx.link().callback(Msg::SelectCity);
+        html! {
+            <>
+                <MapComponent city={&self.city}  />
+                <Control select_city={cb} cities={&self.cities}/>
+            </>
+        }
     }
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<Model>::new().render();
 }
